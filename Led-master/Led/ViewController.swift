8c3d571a73ad74c108eab2aa0dc75a984d6b6446
Tuuu <tu@techmaster.vn>
2016-11-23 10:55:41 +0700

    no message

--- Led-master/Led/ViewController.swift
+++ Led-master/Led/ViewController.swift
@@ -98,8 +98,6 @@ class ViewController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: 0.375, target: self, selector: #selector(moveright), userInfo: nil, repeats: true)
     }
     //12312123
-    //Commit 
-    //hehe
     func moveleft()
     {
         turnOffLed()
