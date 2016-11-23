37f6998cb7a1c4dbedcbc98f442043e49f1e82f6
Tuuu <tu@techmaster.vn>
2016-11-23 10:34:14 +0700

    reverse

--- Led-master/Led/ViewController.swift
+++ Led-master/Led/ViewController.swift
@@ -98,8 +98,8 @@ class ViewController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: 0.375, target: self, selector: #selector(moveright), userInfo: nil, repeats: true)
     }
     //12312123
-    //Commit 
-    
+    //Commit
+    //reverse
     func moveleft()
     {
         turnOffLed()
